96a02bfc95542fe39f8f3e1a0d07a058c3888cf6
Roman Kopaev <roman@kopaev.ru>
2017-02-15 11:54:19 +0300

    ADD Добавлен проект MS CRM / MS CRM 2016 / Plug-in Static State

---
+++ MS CRM/MS CRM 2016/Plug-in Static State/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+
+namespace PZone.Samples
+{
+    /// <summary>
+    /// Проверка сохранения состояния класса плагина между процессами 
+    /// выполнения.
+    /// </summary>
+    /// <remarks>
+    /// Если зарегистрировать плагин на Pre-Update контакта, то будет видно, 
+    /// что при обновлении любого контакта значение, утановленное для поля 
+    /// _testValue сохраняется (для проверки этого, значение записывается в 
+    /// указанное в конфигурации поле).
+    /// Также видно, что для каждого шага (Step) создается отдельный экземпляр 
+    /// класса плагина.
+    /// </remarks>
+    public class StaticStatePlugin : IPlugin
+    {
+        private Guid? _testValue;
+        private readonly string _fieldName;
+
+
+        public StaticStatePlugin(string unsecureConfig)
+        {
+            _fieldName = unsecureConfig;
+        }
+
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var entity = (Entity)context.InputParameters["Target"];
+
+            if (!_testValue.HasValue)
+                _testValue = Guid.NewGuid();
+
+            entity[_fieldName] = _testValue.Value.ToString();
+        }
+    }
+}
