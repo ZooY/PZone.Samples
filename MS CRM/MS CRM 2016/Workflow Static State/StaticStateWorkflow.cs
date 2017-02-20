3f98bc7085e4daa644d25fa328cdfa1cbf5ebd47
Roman Kopaev <roman@kopaev.ru>
2017-02-20 15:10:33 +0300

    ADD Добавлен проект MS CRM / MS CRM 2016 / Workflow Static State

---
+++ MS CRM/MS CRM 2016/Workflow Static State/StaticStateWorkflow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+
+
+namespace PZone.Samples
+{
+    public class StaticStateWorkflow: CodeActivity
+    {
+        private string _message;
+
+
+        [RequiredArgument]
+        [Output("Message")]
+        public OutArgument<string> Message { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            if (string.IsNullOrWhiteSpace(_message))
+                _message = Guid.NewGuid().ToString();
+            Message.Set(context, _message);
+        }
+    }
+}
