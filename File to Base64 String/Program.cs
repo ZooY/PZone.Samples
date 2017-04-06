759906c0900ca8634eb57f57270a2cd0dc0024ba
Roman Kopaev <roman@kopaev.ru>
2017-04-06 16:15:02 +0300

    ADD Добавлен пример преобразования файла в строку Base64

---
+++ File to Base64 String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+
+namespace PZone.Samples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: File2Base64.exe <file-path>");
+            }
+            else
+            {
+                var filePath = args[0];
+
+                var fileBytes = File.ReadAllBytes(filePath);
+                var fileContent = Convert.ToBase64String(fileBytes);
+                Console.WriteLine(fileContent);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
