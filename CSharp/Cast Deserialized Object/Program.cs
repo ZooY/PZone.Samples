5bb8636713ecef5decca44dda4c57c2b75714876
Roman Kopaev <roman@kopaev.ru>
2017-10-13 11:24:25 +0300

    no message

--- CSharp/Cast Deserialized Object/Program.cs
+++ CSharp/Cast Deserialized Object/Program.cs
@@ -6,41 +6,41 @@ using Newtonsoft.Json.Linq;
 
 namespace PZone.Samples
 {
-    class Program
+class Program
+{
+    static void Main()
     {
-        static void Main()
+        // Формируем список элементов ключ-значение, где в качестве ключа выступает тип класса значения.
+        var items = new List<KeyValuePair<Type, MyClassBase>>
         {
-            // Формируем список элементов ключ-значение, где в качестве ключа выступает тип класса значения.
-            var items = new List<KeyValuePair<Type, MyClassBase>>
-            {
-                new KeyValuePair<Type, MyClassBase>(typeof(MyFirstClass), new MyFirstClass("Base", "First")),
-                new KeyValuePair<Type, MyClassBase>(typeof(MySecondClass), new MySecondClass("Base", "Second"))
-            };
+            new KeyValuePair<Type, MyClassBase>(typeof(MyFirstClass), new MyFirstClass("Base", "First")),
+            new KeyValuePair<Type, MyClassBase>(typeof(MySecondClass), new MySecondClass("Base", "Second"))
+        };
 
-            // Сериализуем список в строку.
-            var str = JsonConvert.SerializeObject(items, Formatting.Indented);
-            Console.WriteLine(str);
+        // Сериализуем список в строку.
+        var str = JsonConvert.SerializeObject(items, Formatting.Indented);
+        Console.WriteLine(str);
 
-            // Восстанавливаем список из строки. Типы значений разные и заранее не известны, 
-            // поэтому в качестве типа значения используем тип JObject. Используется именно тип 
-            // JObject, а не Object, потому что у него есть средства приведения типов.
-            var itemsTmp = JsonConvert.DeserializeObject<List<KeyValuePair<Type, JObject>>>(str);
+        // Восстанавливаем список из строки. Типы значений разные и заранее не известны, 
+        // поэтому в качестве типа значения используем тип JObject. Используется именно тип 
+        // JObject, а не Object, потому что у него есть средства приведения типов.
+        var itemsTmp = JsonConvert.DeserializeObject<List<KeyValuePair<Type, JObject>>>(str);
 
-            // Формируем исходный список с нужными типами.
-            items = new List<KeyValuePair<Type, MyClassBase>>();
-            foreach (var pair in itemsTmp)
-            {
-                var item = (MyClassBase)pair.Value.ToObject(pair.Key);
-                items.Add(new KeyValuePair<Type, MyClassBase>(pair.Key, item));
-            }
-
-            // Проверяем, что все типы восстановлены правильно.
-            foreach (var pair in items)
-                Console.WriteLine($"{pair.Key.FullName} {pair.Value.Do()}");
-
-            Console.WriteLine();
-            Console.WriteLine("Press any key...");
-            Console.ReadKey();
+        // Формируем исходный список с нужными типами.
+        items = new List<KeyValuePair<Type, MyClassBase>>();
+        foreach (var pair in itemsTmp)
+        {
+            var item = (MyClassBase)pair.Value.ToObject(pair.Key);
+            items.Add(new KeyValuePair<Type, MyClassBase>(pair.Key, item));
         }
+
+        // Проверяем, что все типы восстановлены правильно.
+        foreach (var pair in items)
+            Console.WriteLine($"{pair.Key.FullName} {pair.Value.Do()}");
+
+        Console.WriteLine();
+        Console.WriteLine("Press any key...");
+        Console.ReadKey();
     }
+}
 }
