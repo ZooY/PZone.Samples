f38e49333ef5504c1ccb3a90660f550ea7cb6d3f
Roman Kopaev <roman@kopaev.ru>
2018-02-07 11:06:27 +0300

    no message

---
+++ CheckAvailability/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Security;
+
+
+namespace CheckAvailability
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region Доступность физического сервера
+
+
+            {
+
+                var host = "crmdevapp02";
+                var available = ServerAvailability(host);
+                Console.WriteLine($"Server {host}: {(available ? "OK" : "Not available")}");
+
+            }
+
+
+            #endregion
+
+
+            #region Доступность IIS
+
+
+            {
+
+                var host = "crmdevapp02";
+                var available = IisAvailability(host);
+                Console.WriteLine($"IIS on server {host}: {(available ? "OK" : "Not available")}");
+
+            }
+
+
+            #endregion
+
+
+            #region Доступность веб-сайта
+
+
+            {
+                var site = "http://crmdevapp01/DEV01";
+                var available = SiteAvailability(site);
+                Console.WriteLine($"Site {site}: {(available ? "OK" : "Not available")}");
+            }
+
+
+            #endregion
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+
+
+        private static bool ServerAvailability(string host)
+        {
+            bool available;
+            using (var pingSender = new System.Net.NetworkInformation.Ping())
+            {
+                try
+                {
+                    pingSender.Send(host);
+                    available = true;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                    available = false;
+                }
+            }
+            return available;
+        }
+
+
+        private static bool IisAvailability(string host)
+        {
+            var available = false;
+
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo("iisreset", $"{host} /status")
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                var line = proc.StandardOutput.ReadLine();
+                if (line != null && line.Contains("IISADMIN"))
+                    available = true;
+            }
+            return available;
+        }
+
+
+        private static bool SiteAvailability(string uri)
+        {
+            bool available;
+            try
+            {
+                var request = WebRequest.Create(uri);
+                request.Credentials = CredentialCache.DefaultCredentials;
+                var response = (HttpWebResponse)request.GetResponse();
+                available = response.StatusCode == HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                available = false;
+            }
+            return available;
+        }
+    }
+}
---
+++ MS CRM/MS CRM 2016/Picklist Items/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+
+
+namespace PZone.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceUrl = "http://crmdevapp01/DEV01/XRMServices/2011/Organization.svc";
+
+            var credentials = new ClientCredentials();
+            credentials.Windows.ClientCredential = CredentialCache.DefaultNetworkCredentials;
+            using (var service = new OrganizationServiceProxy(new Uri(serviceUrl), null, credentials, null))
+            {
+                // Значения списка атрибута сущности
+                {
+                    var request = new RetrieveAttributeRequest
+                    {
+                        EntityLogicalName = "contact",
+                        LogicalName = "paymenttermscode"
+                    };
+                    var response = (RetrieveAttributeResponse)service.Execute(request);
+                    var attributeMetadata = (PicklistAttributeMetadata)response.AttributeMetadata;
+                    Console.WriteLine($"Entity {request.EntityLogicalName}, picklist attribute {request.LogicalName}:");
+                    foreach (var option in attributeMetadata.OptionSet.Options)
+                        Console.WriteLine($"{option.Value} - {option.Label.UserLocalizedLabel.Label}");
+                }
+
+                // Значения глобального списка
+                {
+                    var request = new RetrieveOptionSetRequest
+                    {
+                        Name = "budgetstatus"
+                    };
+                    var response = (RetrieveOptionSetResponse)service.Execute(request);
+                    var attributeMetadata = (OptionSetMetadata)response.OptionSetMetadata;
+                    Console.WriteLine($"Global picklist {request.Name}:");
+                    foreach (var option in attributeMetadata.Options)
+                        Console.WriteLine($"{option.Value} - {option.Label.UserLocalizedLabel.Label}");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
