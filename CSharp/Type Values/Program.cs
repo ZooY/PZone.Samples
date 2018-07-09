f3d4e617fc9dd0b5ee5213e39a8ca9fb604b92d2
Roman Kopaev <roman@kopaev.ru>
2018-07-09 10:32:17 +0300

    no message

---
+++ CSharp/Type Values/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace PZone.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pad = 20;
+
+            Console.WriteLine(($"=== {typeof(int).Name} ===").PadRight(pad, '='));
+            Console.WriteLine($"Min: {int.MinValue:0,0}");
+            Console.WriteLine($"Max: {int.MaxValue:0,0}");
+            Console.WriteLine();
+
+            Console.WriteLine(($"=== {typeof(decimal).Name} ===").PadRight(pad, '='));
+            Console.WriteLine($"Min: {decimal.MinValue:#,#.#}");
+            Console.WriteLine($"Max: {decimal.MaxValue:#,#.#}");
+            for (var i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Кол-во десятичных знаков - {i}");
+                Console.WriteLine($"   Min: {decimal.MinValue / (decimal)Math.Pow(10, i):#,#.##########}");
+                Console.WriteLine($"   Max: {decimal.MaxValue / (decimal)Math.Pow(10, i):#,#.##########}");
+            }
+            Console.WriteLine($"Optimal Min: {-7000000000000000000.0000000000m:#,#.0000000000}");
+            Console.WriteLine($"Optimal Max: {7000000000000000000.0000000000m:#,#.0000000000}");
+            Console.WriteLine();
+
+            Console.WriteLine(($"=== {typeof(double).Name} ===").PadRight(pad, '='));
+            Console.WriteLine($"Min: {double.MinValue:#,#.#}");
+            Console.WriteLine($"Max: {double.MaxValue:#,#.#}");
+            Console.WriteLine($"Optimal Min: {-10000000000.00000d:#,#.00000}");
+            Console.WriteLine($"Optimal Max: { 10000000000.00000d:#,#.00000}");
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Web/HTML/Change Background On Scroll/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Change_Background_On_Scroll")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Change_Background_On_Scroll")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f71b2193-42dc-4d63-9ca5-b656082ce838")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
