1817f6561215bc5e5ba782e85e5a924112348141
Роман Копаев <roman@kopaev.ru>
2018-08-22 09:55:48 +0300

    no message

---
+++ MS CRM/MS CRM 2016/Connect to CRM/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.ServiceModel.Description;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace PZone.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // On-premises, Integrated security
+            var serviceUrl = "https://crmtest/dev01/XRMServices/2011/Organization.svc";
+            var credentials = new ClientCredentials
+            {
+                Windows = { ClientCredential = CredentialCache.DefaultNetworkCredentials }
+            };
+            using (var service = new OrganizationServiceProxy(new Uri(serviceUrl), null, credentials, null))
+            {
+                // Execute request to CRM
+                var currentUser = (WhoAmIResponse)service.Execute(new WhoAmIRequest());
+                Console.WriteLine($"User ID: {currentUser.UserId}");
+                Console.WriteLine($"Organization ID: {currentUser.OrganizationId}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -32,6 +32,7 @@
     * [Web API OWIN](https://github.com/ZooY/PZone.Samples/tree/master/Web/Web%20API/Web%20API%20OWIN) - Пример создания "чистого" проекта Web API на OWIN.
 * **MS CRM** 
   * MS CRM 2016 
+    * [Connect to CRM](https://github.com/ZooY/PZone.Samples/tree/master/MS%20CRM/MS%20CRM%202016/COnnect%20to%20CRM) - Пример подключения к CRM 2016/D365.
     * [Create Annotation](https://github.com/ZooY/PZone.Samples/tree/master/MS%20CRM/MS%20CRM%202016/Create%20Annotation) - Пример создания примечания, в том числе с вложением.
     * [Discovery Service Connection](https://github.com/ZooY/PZone.Samples/tree/master/MS%20CRM/MS%20CRM%202016/Discovery%20Service%20Connection) - Пример настройки подключения к сервису обнаружения (Discovery Service) в Microsoft Dynamics CRM 2016.
     * [Entity Metadata](https://github.com/ZooY/PZone.Samples/tree/master/MS%20CRM/MS%20CRM%202016/Entity%20Metadata) - Пример получения метаданных сущностей CRM.
