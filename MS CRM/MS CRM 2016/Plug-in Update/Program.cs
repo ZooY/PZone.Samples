0b5c003f5ac786303937c54abd0258685e7de13f
Роман Копаев <roman@kopaev.ru>
2018-08-31 12:31:51 +0300

    no message

--- MS CRM/MS CRM 2016/Plug-in Update/Program.cs
+++ MS CRM/MS CRM 2016/Plug-in Update/Program.cs
@@ -15,11 +15,12 @@ namespace PZone.Samples
         static void Main(string[] args)
         {
             var assemblyFileName = @"C:\PZone.MyPlugin.dll";
-            var serviceUrl = "https://crmtest.tpcorp.ru/TEST/XRMServices/2011/Organization.svc";
+            var serviceUrl = "https://crmtest/TEST/XRMServices/2011/Organization.svc";
 
-            var assemblyName = AssemblyName.GetAssemblyName(assemblyFileName);
-            var assemblyVersion = $"{assemblyName.Version.Major}.{assemblyName.Version.Minor}";
-            var assemblyPublicKeyToken = GetPublicKeyToken(assemblyName);
+            var assemblyNameInfo = AssemblyName.GetAssemblyName(assemblyFileName);
+            var assemblyName = assemblyNameInfo.Name;
+            var assemblyVersion = $"{assemblyNameInfo.Version.Major}.{assemblyNameInfo.Version.Minor}";
+            var assemblyPublicKeyToken = GetPublicKeyToken(assemblyNameInfo);
 
             var credentials = new ClientCredentials { Windows = { ClientCredential = CredentialCache.DefaultNetworkCredentials } };
             using (var service = new OrganizationServiceProxy(new Uri(serviceUrl), null, credentials, null))
@@ -27,12 +28,12 @@ namespace PZone.Samples
                 // Find Plug-in
                 var query = new QueryExpression("pluginassembly")
                 {
-                    ColumnSet = new ColumnSet("name", "publickeytoken", "version", "culture"),
+                    ColumnSet = new ColumnSet("pluginassemblyid"),
                     Criteria = new FilterExpression(LogicalOperator.And)
                     {
                         Conditions =
                         {
-                            new ConditionExpression("name", ConditionOperator.Equal, assemblyName.Name),
+                            new ConditionExpression("name", ConditionOperator.Equal, assemblyName),
                             new ConditionExpression("publickeytoken", ConditionOperator.Equal, assemblyPublicKeyToken),
                             new ConditionExpression("version", ConditionOperator.BeginsWith, assemblyVersion),
                         }
@@ -53,10 +54,10 @@ namespace PZone.Samples
         }
 
 
-        private static string GetPublicKeyToken(AssemblyName assemblyName)
+        private static string GetPublicKeyToken(AssemblyName assemblyNameInfo)
         {
             var publicKeyTokenString = "";
-            var token = assemblyName.GetPublicKeyToken();
+            var token = assemblyNameInfo.GetPublicKeyToken();
             for (var i = 0; i < token.GetLength(0); i++)
                 publicKeyTokenString += token[i].ToString("x2");
             return publicKeyTokenString;
